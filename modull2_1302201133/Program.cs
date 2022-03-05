1fd06b205bff23acd4e229e445f4ca7411f9f2b3
fiqril <mfikrilh@gmail.com>
2022-03-04 22:51:27 -0800

    implementasi kalkulator

---
+++ modull2_1302201133/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace modull2_1302201133
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
